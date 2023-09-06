a5623718180ff5548579d57c558df26fa9522534
jihyun <j.hyun2658@gmail.com>
2023-09-06 21:43:05 +0900

    day0906

--- etc/backjune/src/backjoon/Main.java
+++ etc/backjune/src/backjoon/Main.java
@@ -1,19 +1,12 @@
 package backjoon;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
-		int a = sc.nextInt();
-		int min = 1000000;
-		int max = -1000000;
-		for(int i =0; i < a; i++) {
-			int b = sc.nextInt();
-			if(b < min) min = b;
-			if(b > max) max = b;
-		}
-		System.out.printf("%d %d", min, max);
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String s[] = br.readLine().strip().split(" ");
+		System.out.println(s.length);
 	}
 }
