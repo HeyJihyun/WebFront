02e108646360bafb4dd3894468505ed76dd9eb26
jihyun <j.hyun2658@gmail.com>
2023-09-12 21:29:58 +0900

    day0912 자바스크립트 class, react 입문

---
+++ react-workspace/basic_app/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Hello from './Hello';
+
+function App() {
+  return (
+    <div className="App">
+      /* <h1>Hello React</h1> */
+      <Hello />
+    </div>
+  );
+}
+
+export default App;
